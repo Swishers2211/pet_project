f5932c5767a444802f8d732b1781f8fab53af705
Maxim Kudeikin <mauricevvs@gmail.com>
2024-03-29 21:26:48 +0300

    added gunicorn + nginx

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+
+gunicorn --bind 0.0.0.0:8000 masters_guild.wsgi
+# python manage.py runserver 0.0.0.0:8000
